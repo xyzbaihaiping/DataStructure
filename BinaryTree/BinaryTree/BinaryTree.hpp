474db2f59fc956a58923b0b3453d3b668065cec8
xyzbaihaiping <bhp16333177100@sina.com>
2016-04-26 11:13:35 +0800

    二叉树及成员函数的实现

--- BinaryTree/BinaryTree/BinaryTree.hpp
+++ BinaryTree/BinaryTree/BinaryTree.hpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<assert.h>
 #include<queue>
+#include<stack>
 using namespace std;
 
 template<class T>
@@ -65,42 +66,108 @@ public:
 		}
 		return head;
 	}
-	void PreOrder()//先序遍历
+	//void PreOrder()//递归实现先序遍历
+	//{
+	//	_PreOrder(_root);
+	//	cout << endl;
+	//}
+	//void InOrder()//递归实现中序遍历
+	//{
+	//	_InOrder(_root);
+	//	cout << endl;
+	//}
+	//void PostOrder()//递归实现后序遍历
+	//{
+	//	_PostOrder(_root);
+	//	cout << endl;
+	//}
+	void PrevOrderNonR()//非递归
 	{
-		_PreOrder(_root);
+		stack<Node*> s; //利用栈的先进后出，后进先出的性质保存二叉树节点
+		Node* cur = _root;
+		if (cur == NULL)
+			return;
+		s.push(cur);//若根节点不为空，则将根节点入栈
+		
+		while (!s.empty())//栈为空表示二叉树已访问完
+		{
+			cur = s.top();//访问栈顶的元素
+			cout << cur->_data << " ";
+			s.pop();//栈顶元素出栈
+			if (cur->_right)//将访问过的元素的右节点压栈（后访问的先压栈）
+				s.push(cur->_right);
+			if (cur->_left)//左节点压栈
+				s.push(cur->_left);
+		}
+
 		cout << endl;
 	}
-	void InOrder()//中序遍历
+	void InOrderNonR()//非递归
 	{
-		_InOrder(_root);
+		stack<Node*> s;
+		Node* cur = _root;
+		while (cur || !s.empty())//若节点不为空或栈不为空
+		{
+			while(cur)//若节点不为空
+			{
+				s.push(cur);//将节点入栈
+				cur = cur->_left;//若该节点有左节点，则将左节点入栈
+			}
+			cur = s.top();
+			cout << cur->_data << " ";//访问最左节点（代表该节点左节点为空或已访问）
+			s.pop();
+			cur = cur->_right;//访问右节点
+		}
+
 		cout << endl;
 	}
-	void PostOrder()//后序遍历
+
+	void PostOrderNonR()//非递归
 	{
-		_PostOrder(_root);
+		stack<Node*> s;
+		Node* cur = _root;
+		Node* prev = NULL;//上一个访问过的节点
+
+		while (cur || !s.empty())//若节点不为空或栈不为空
+		{
+			while(cur)
+			{
+				s.push(cur);
+				cur = cur->_left;
+			}//栈顶元素为最左节点，第一个访问
+			Node* top= s.top();//取到栈顶元素
+			if (top->_right == NULL || prev == top->_right)//若栈顶元素的右节点为空，或它的右节点已访问过，则访问该节点
+			{
+				cout << top->_data << " ";
+				s.pop();
+				prev = top;//更新prev
+			}
+			else
+				cur = top->_right;//若给元素的右节点没有访问过，则将该元素的右节点压栈
+		}
 		cout << endl;
 	}
+	//思路：利用队列先进先出的特点,访问上一层节点时，将它的下一层节点入队，访问完上一层节点后，
+	//将其出队，然后可访问下一层节点
 	void LevelOrder()//层序遍历
 	{
-		queue<Node*> q1;//永远保存当前打印的一层所有节点
-		queue<Node*> q2;//永远保存当前一层节点的子节点
-		q1.push(_root); //根节点压入队列
-
-		while (q1.size())//若当前一层节点没有打印完
+		queue<Node*> q;
+		Node* cur = _root;
+		if (cur)
+			q.push(cur);//若根节点不为空，将根节点入队
+		while (!q.empty())//层序遍历完的结束条件是队列为空
 		{
-			cout << (q1.front())->_data << " ";//q1队头的节点是将要打印的节点
-
-			if (q1.front()->_left != NULL)//若该节点的左孩子不为空
-				q2.push(q1.front()->_left);//将该结点左孩子入队q2
-			if (q1.front()->_right != NULL)//若该节点的右孩子不为空
-				q2.push(q1.front()->_right);//将该结点左孩子入队q2
-
-			q1.pop();//将打印过后的节点出队后，队头的元素为该层下一个未打印的节点
-			if (q1.size() == 0)//若该层节点全部打印完
+			cur = q.front();
+			cout <<cur->_data << " ";//访问队头元素
+			if (cur->_left)
 			{
-				swap(q1, q2);//交换q1,q2,将下一层的节点保存在要打印的队列中
-				continue;//继续打印
+				q.push(cur->_left);//将该元素的左节点入队
 			}
+			if (cur->_right)
+			{
+				q.push(cur->_right);//将该元素的右节点入队
+			}
+			q.pop();//访问过的节点出队
 		}
 		cout << endl;
 	}
@@ -115,6 +182,21 @@ public:
 	size_t LeafSize()//叶子节点个数
 	{
 		return _LeafSize(_root);
+	}
+	Node* Find(const T& x)//查找元素
+	{
+		return _Find(_root,x);
+	}
+	/*size_t GetKlevel(size_t k)
+	{
+		size_t size = 0;
+		size_t level = 1;
+		_GetKlevel(_root, k, level, size);
+		return size;
+	}*/
+	size_t GetKlevel(size_t k)
+	{
+		return _GetKlevel(_root,k);
 	}
 protected:
 	void Destry(Node* root)
@@ -180,46 +262,70 @@ protected:
 	
 	size_t _Size(Node* root)
 	{
-		int size = 0;
-		if (root)//节点不为空时，size++
-		{
-			size++;
-			size+=_Size(root->_left);
-			size+=_Size(root->_right);
-		}
-		return size;
+		if (root == NULL)
+			return 0;
+		return 1 + _Size(root->_left) + _Size(root->_right);
 	}
 
-	//深度
-	//思路：默认每一层深度为1，递归到最后一层后往上返回（每返回一层深度+1）
+	//深度(高度)
+	//思路：若根节点不为空，则二叉树深度取左子树深度和右子数深度中的最大值，
 	size_t _Depth(Node* root)
 	{
-		size_t depth=1;//最深深度，默认若节点不为空则深度为1
-		if (root)//不为空
+		if (root == NULL)
 		{
-			size_t trdepth = _Depth(root->_left);//递归计算左子树的深度
-			if (trdepth + 1 > depth)//若左子树深度大于当前深度
-				depth = trdepth + 1;//重置最深深度
-			trdepth = _Depth(root->_right);//递归计算右子树深度
-			if (trdepth + 1 > depth)
-				depth = trdepth + 1;//重置最深深度
+			return 0;
+		}
+		size_t Ldepth = _Depth(root->_left);//计算左子树深度
+		size_t Rdepth = _Depth(root->_right);//计算右子树深度
+		if (Ldepth > Rdepth)//取最深的值
+		{
+			return Ldepth+1;//当前深度是左子树深度+1
 		}
 		else
-			depth = 0;//若为节点是空节点，深度为0
-		return depth;
+			return Rdepth+1;//当前深度是右子树深度+1
 	}
 	size_t _LeafSize(Node* root)//叶子节点个数
 	{
-		size_t size = 0;
 		if (root == NULL)//为空返回
-			return size;
+			return 0;
 		if (root->_left == NULL && root->_right == NULL)//若节点为叶子节点（没有孩子）
-			return ++size;//叶子节点个数+1
+			return 1;
+		return _LeafSize(root->_left) + _LeafSize(root->_right);//叶子节点的个数是左子树的叶子节点个数+右子树叶子节点的个数
+	}
+	Node* _Find(Node* root, const T& x)//查找节点
+	{
+		if (root == NULL)//若节点为空
+			return NULL;//返回空
+		if (root->_data == x)//若节点值等于要查找的值
+			return root;//返回该节点
+		Node* ret = _Find(root->_left, x);//若不等于，则递归在左子树中查找
+		if (ret)
+		        return ret;//若查找结果不为空，则返回查找的节点地址
 		else
-		{
-			size+=_LeafSize(root->_left);//递归寻找左子树的叶子节点
-			size+=_LeafSize(root->_right);//递归寻找右子数的叶子节点
-		}
+			return _Find(root->_right, x);//若为空（代表左子树中没有该节点），则在右子树中查找
+	
+	}
+	//void _GetKlevel(Node* root, size_t k,size_t level,size_t& size)//第k层节点数，传值法
+	//{
+	//	if (root == NULL)
+	//		return;
+	//	if (level == k)//若k等于当前层数
+	//	{
+	//		++size;
+	//		return ;//返回size+1;
+	//	}
+	//	_GetKlevel(root->_left, k, level + 1, size);//访问下一层
+	//	_GetKlevel(root->_right, k, level + 1, size);//访问下一层
+
+	//}
+	size_t _GetKlevel(Node* cur,size_t k)
+	{
+		if (cur == NULL)
+			return 0;
+		if (k == 1)
+			return 1;
+		else
+			return _GetKlevel(cur->_left, k - 1) + _GetKlevel(cur->_right, k - 1);
 	}
 protected:
 	BinaryTreeNode<T>* _root;
--- BinaryTree/BinaryTree/test.cpp
+++ BinaryTree/BinaryTree/test.cpp
@@ -7,19 +7,21 @@ void TestBinaryTree()
 	int array2[15] = { 1, 2, '#', 3, '#', '#', 4, 5, '#', 6, '#', 7, '#', '#', 8 };
 	int size = sizeof(array1) / sizeof(array1[0]);
 	BinaryTree<int> tree(array1, '#', size);
-	tree.PreOrder();//先根遍历
-	tree.InOrder();//中序遍历
-	tree.PostOrder();//后根遍历
+	tree.PrevOrderNonR();//先根遍历
+	tree.InOrderNonR();//中序遍历
+	tree.PostOrderNonR();//后根遍历
 	tree.LevelOrder();//层序遍历
 	cout << "size:"<<tree.size() << endl;//二叉树中元素个数
 	cout << "depth："<<tree.Depth() << endl;//二叉树深度
 	cout << "叶子节点个数："<<tree.LeafSize() << endl;//二叉树叶子节点个数
-
-	BinaryTree<int> tree1(tree);//拷贝
-	tree.PreOrder();
-	BinaryTree<int> tree3;
-	tree3 = tree;//赋值
-	tree3.PreOrder();
+	cout << "Find 4：" << tree.Find(4)->_data<< endl;
+	cout << "Find 7: " << tree.Find(7) << endl;
+	cout << "GetKlevel3:" << tree.GetKlevel(3) << endl;
+	//BinaryTree<int> tree1(tree);//拷贝
+	//tree.PreOrder();
+	//BinaryTree<int> tree3;
+	//tree3 = tree;//赋值
+	//tree3.PreOrder();
 }
 
 int main()
