42402de61fbcb8ee1d811912e98620bdd1d31d29
xyzbaihaiping <bhp16333177100@sina.com>
2016-06-08 00:17:10 +0800

    另一种方法

--- 29数组中出现次数超过一半的数字/29数组中出现次数超过一半的数字/test.cpp
+++ 29数组中出现次数超过一半的数字/29数组中出现次数超过一半的数字/test.cpp
@@ -18,8 +18,24 @@ bool CheckMoreThanHalf(int *a, int length,int number)
 	}
 	return true;
 }
+int Partition(int *a, int length, int start, int end)
+{
+	assert(a != NULL || length > 0 || start >=0 || end < length);
+	int prev = start - 1;
+	for (int i = start; i < end; i++)
+	{
+		if (a[i] < a[end])
+		{
+			prev++;
+			if (prev != i)
+				swap(a[prev], a[i]);
+		}
 
-int MoreThanHalfNum(int *a, int length)
+	}
+	swap(a[++prev],a[end]);
+	return prev;
+}
+int MoreThanHalfNum(int *a, int length)//法一，基于partition函数的O(N)算法，排列数组以数组中间的值为基准左边都<=该值，右边>=该值
 {
 	bool g_InputInvalid = false;
 	if (a == NULL || length <= 0)
@@ -27,10 +43,11 @@ int MoreThanHalfNum(int *a, int length)
 		g_InputInvalid = true;
 		return 0;
 	}
-	int middle = length >> 2;
+	int middle = length >> 1;
 	int start = 0;
 	int end = length - 1;
-	int index = 0;
+	int index = Partition(a, length, start, end);
+
 	while (index!=middle)
 	{
 		if (index > middle)
